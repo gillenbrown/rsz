c9a30b1974e607f70671540076d99b3afd8d5a72
diabolique- <gillen314@gmail.com>
2015-05-01 16:04:33 -0500

    start of ch1-ch2 redshift finding
    Started the code. All it currently does is read the parameter file. It
    should be runnable from the command line, which is nice.

---
+++ main.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+def parse_config():
+    """Parse the config file, making a dictionary of parameters
+
+    :return: dictionary, with keys of parameter names, and values of the desired values for those parameters.
+    """
+    global params
+    params = dict()
+    config_file = open("config.txt")
+    for line in config_file:
+        if not line.startswith("#") and not line.isspace():
+            params[line.split()[0]] = line.split()[2]
+    config_file.close()
+    return params
+
+
+def main():
+    # parse the config file
+    params = parse_config()
+
+if __name__ == "__main__":
+    main()
